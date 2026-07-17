c26de702fd6d136d598422425c003e779a081000
hinklab <botserver171@gmail.com>
2026-07-18 01:27:48 +0500

    Update main.jsx

--- renderer/src/main.jsx
+++ renderer/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
+import api from './config/api.js'
+
+if (!window.api) {
+  window.api = api
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
